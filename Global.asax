1f5715a7bc5cf742003daf566b0b0f91788a2bfc
rakinfy <44544322+rakinfy@users.noreply.github.com>
2018-11-13 19:43:55 +0100

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Patient_API.WebApiApplication" Language="C#" %>
---
+++ PatientTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Xml.Linq;
+using Patient_API.Controllers;
+namespace Patient_Test
+{
+    [TestClass]
+    public class PatientTest
+    {
+        [TestMethod]
+        public void GetTest()
+        {
+            // Arrange
+            //Act
+            //Assert
+
+
+
+
+            string target = @"< ForeName > Shilpi </ ForeName >< Surname > Kumari </ Surname >< DateOfBirth > 0001 - 01 - 01T00: 00:00 </ DateOfBirth >  < Gender > Female </ Gender > < Telephones > < Telephone >< PhoneType > Home </ PhoneType ></ Telephone >< Telephone >   < PhoneType > Work </ PhoneType > </ Telephone > < Telephone > < PhoneType > Mobile </ PhoneType > < PhoneNumber > 4356567 </ PhoneNumber ></ Telephone ></ Telephones ></ PatientDetails > ";
+            string Actual = string.Empty;
+            //  Actual=Patient_API.Controllers.PatientController.Equals(target);
+            Assert.AreEqual(target, Actual);
+        }
+        [TestMethod]
+        public void PostTest()
+        {
+
+           
+        }
+        
+    }
+}
